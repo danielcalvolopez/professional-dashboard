9814d627e7fce453b40c5f20e53d0805c8dbf108
Daniel <acidistrict@gmail.com>
2022-10-24 20:21:31 +0100

    pyramid and stacked charts implemented

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,11 +1,6 @@
-import { BsCurrencyDollar } from "react-icons/bs";
 import { GoPrimitiveDot } from "react-icons/go";
-import { Stacked, Pie, Button, SparkLine } from "../components";
-import {
-  earningData,
-  SparklineAreaData,
-  ecomPieChartData,
-} from "../data/dummy";
+import { Stacked, Button, SparkLine } from "../components";
+import { earningData, SparklineAreaData } from "../data/dummy";
 import { useStateContext } from "../context/ContextProvider";
 
 const Ecommerce = () => {
