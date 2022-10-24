2dfd1fb790b95450487f44855d4ea6f02fc1c426
Daniel <acidistrict@gmail.com>
2022-10-24 20:05:08 +0100

    pie chart implemented

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,5 +1,55 @@
+import {
+  Category,
+  ChartComponent,
+  ColumnSeries,
+  DataLabel,
+  Inject,
+  Legend,
+  SeriesCollectionDirective,
+  SeriesDirective,
+} from "@syncfusion/ej2-react-charts";
+import { Tooltip } from "@syncfusion/ej2-react-popups";
+import { Header } from "../../components";
+import {
+  barCustomSeries,
+  barPrimaryXAxis,
+  barPrimaryYAxis,
+} from "../../data/dummy";
+
+import { useStateContext } from "../../context/ContextProvider";
+
 const Bar = () => {
-  return <div>Bar</div>;
+  const { currentMode } = useStateContext();
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Bar" title="Olympic Medal Counts - RIO" />
+      <div className="w-full">
+        <ChartComponent
+          id="charts"
+          primaryXAxis={barPrimaryXAxis}
+          primaryYAxis={barPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true }}
+          background={currentMode === "Dark" ? "#33373E" : "#fff"}
+          legendSettings={
+            currentMode === "Light"
+              ? { background: "white" }
+              : { background: "gray" }
+          }
+        >
+          <Inject
+            services={[ColumnSeries, Legend, Tooltip, Category, DataLabel]}
+          />
+          <SeriesCollectionDirective>
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            {barCustomSeries.map((item, index) => (
+              <SeriesDirective key={index} {...item} />
+            ))}
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
+  );
 };
 
 export default Bar;
--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,18 +1,20 @@
 import { pieChartData } from "../../data/dummy";
 import { Header, Pie as PieChart } from "../../components";
 
-const Pie = () => (
-  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-    <Header category="Pie" title="Project Cost Breakdown" />
-    <div className="w-full">
-      <PieChart
-        id="chart-pie"
-        data={pieChartData}
-        legendVisiblity
-        height="full"
-      />
+const Pie = () => {
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Chart" title="Inflation Rate" />
+      <div className="w-full">
+        <PieChart
+          id="chart-pie"
+          data={pieChartData}
+          legendVisiblity
+          height="full"
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Pie;
